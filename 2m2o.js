4cb6137604888a84b1a6df34e277b6beb2e116c3
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2020-12-25 14:23:12 +0700

    Update 2m2o.js

--- 2m2o.js
+++ 2m2o.js
@@ -17,6 +17,7 @@ client.on('ready', () => {
 client.on("message", message => {
 	if(message.author == client.user) return;
 
+	// Status of bot
 	client.user.setPresence({
 		status: "online",
 		game: {
@@ -24,6 +25,55 @@ client.on("message", message => {
 		type: "PLAYING"
 		}
 	});
+
+	// Forked from Foundation Bot
+		// Check if channel
+		let botChat  = client.channels.get('783648498876022814');
+		let otherChat  = client.channels.get('783648482862301184');
+
+			// Constant
+		  const args = message.content.slice('').trim().split(/ +/g);
+		  const command = args.shift().toLowerCase();
+				// Check guild
+				const guild = message.guild.id === '783647633457020969';
+
+		if(!guild) return;
+			  // delete message on cats, ,s
+					if(command === "cats") {
+						// Timing
+						console.log("working 1")
+
+						setTimeout(function(){
+							message.delete();
+						}, 3000);// 30 secon ds
+
+					}
+			// too
+				if(command === ",s") {
+					console.log("working")
+					// Timing
+					setTimeout(function(){
+						message.delete();
+					}, 3000); // 30 seconds
+
+				}
+
+			// On bot ping
+			if(message.isMentioned(client.user)) {
+
+				const embed = new Discord.RichEmbed()
+										.setColor(0x000DFF)
+										.setTitle('[Bot Command]')
+										.setDescription('Không có bất kì lệnh bổ sung nào.')
+										.setFooter("Dev by Moonz#0001")
+										.setTimestamp();
+
+				message.channel.send(embed).then(message => {
+					message.delete(10000);
+				});
+
+			}
+
 
 });
 
